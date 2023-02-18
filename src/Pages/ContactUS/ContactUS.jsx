18b9179194eab242d2a0f834691cf89aa4d361e1
Kazi Rahat <kazirahat1007@outlook.com>
2023-02-18 17:53:59 +0600

    update teams page

--- src/Pages/ContactUS/ContactUS.jsx
+++ src/Pages/ContactUS/ContactUS.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const ContactUS = () => {
-    const teams
+   const teams = [];
+
    return (
       <div className="text-center">
          <h1 className="text-4xl">Awesome Teams</h1>
